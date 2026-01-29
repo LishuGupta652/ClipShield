d4b147296473723921bd86a2ae0835228e2d07b0
lishugupta652 <lishugupta652@gmail.com>
2026-01-29 21:59:44 +0530

    Update URLs in mactools.rb to point to the correct GitHub repository

--- Casks/mactools.rb
+++ Casks/mactools.rb
@@ -2,10 +2,10 @@ cask "mactools" do
   version "1.0.0"
   sha256 "8e56ac0310281aeb45ae391caffdbc8fe4f57edb55052668b47decfdc4c7fe18"
 
-  url "https://github.com/yourname/mactools/releases/download/v#{version}/MacTools-#{version}.zip"
+  url "https://github.com/LishuGupta652/macos-toolkit/releases/download/1.0.0/MacTools-#{version}.zip"
   name "MacTools"
   desc "Personal macOS menu bar toolkit"
-  homepage "https://github.com/yourname/mactools"
+  homepage "https://github.com/LishuGupta652/macos-toolkit"
 
   depends_on macos: ">= :ventura"
 
